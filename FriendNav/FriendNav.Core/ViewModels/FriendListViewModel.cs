1b3adb4886df439ca9ca75517f83ff9e2ea0b0e3
scch1002 <chrisschwan8009@hotmail.com>
2018-01-07 14:06:24 -0600

    Changed to use navigation service

--- FriendNav/FriendNav.Core/ViewModels/FriendListViewModel.cs
+++ FriendNav/FriendNav.Core/ViewModels/FriendListViewModel.cs
@@ -1,11 +1,16 @@
-﻿using MvvmCross.Core.ViewModels;
+﻿using FriendNav.Core.Model;
+using MvvmCross.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FriendNav.Core.ViewModels
 {
-    public class FriendListViewModel : MvxViewModel
+    public class FriendListViewModel : MvxViewModel<User>
     {
+        public override void Prepare(User parameter)
+        {
+            
+        }
     }
 }
