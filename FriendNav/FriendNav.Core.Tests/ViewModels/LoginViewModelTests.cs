2d5e39f5845cbad4ca2ca07cb8cca110c5f50e95
scch1002 <chrisschwan8009@hotmail.com>
2018-01-28 14:02:40 -0600

    Created login unit test

--- FriendNav/FriendNav.Core.Tests/ViewModels/LoginViewModelTests.cs
+++ FriendNav/FriendNav.Core.Tests/ViewModels/LoginViewModelTests.cs
@@ -7,40 +7,66 @@ using MvvmCross.Core.Navigation;
 using FriendNav.Core.Repositories.Interfaces;
 using FriendNav.Core.Services.Interfaces;
 using FriendNav.Core.Model;
+using FriendNav.Core.IntegrationTests.Utilities;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Firebase.Auth;
+using FriendNavUser = FriendNav.Core.Model.User;
+using System.Threading.Tasks;
 
 namespace FriendNav.Core.Tests.ViewModels
 {
     [TestClass]
     public class LoginViewModelTests
     {
+        private IFixture _fixture = null;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _fixture = new Fixture()
+                .Customize(new AutoConfiguredMoqCustomization());
+        }
+
         [TestMethod]
         public void User_login_and_navigate_to_FriendList()
         {
-            var _task = new Mock<ITask>();
             var _mvxNavigationService = new Mock<IMvxNavigationService>();
             var _userRepository = new Mock<IUserRepository>();
-            var _notificationService = new Mock<INotificationService>();
             var _firebaseAuthService = new Mock<IFirebaseAuthService>();
+            var firebaseAuth = _fixture.Create<FirebaseAuth>();
+            var user = new FriendNavUser();
 
-            var _chatRepository = new Mock<IChatRepository>(); 
-            var _mvxNavigationServiceFL = new Mock<IMvxNavigationService>();
+            _firebaseAuthService
+                .SetupGet(s => s.FirebaseAuth)
+                .Returns(firebaseAuth);
 
-            // _mvxNavigationService.Setup(x => x.Navigate<FriendListViewModel>())));
-            _firebaseAuthService.Setup(x => x.LoginUser("", "")).Raises(_firebaseAuthService.Object.FirebaseAuth = new Firebase.Auth.FirebaseAuth);
+            _mvxNavigationService.Setup(s => s.Navigate<FriendListViewModel, FriendNavUser>(It.IsAny<FriendNavUser>(), null))
+                .Returns(() => 
+                {
+                    return Task.Run(() => { });
+                });
 
-            var loginViewModel = new LoginViewModel(
-                _task.Object,
+            _userRepository.Setup(s => s.GetUser(It.IsAny<string>()))
+                .Returns(user);
+
+            var sut = new LoginViewModel(
+                new TestTask(),
                 _mvxNavigationService.Object,
                 _userRepository.Object,
-                _notificationService.Object,
+                null,
                 _firebaseAuthService.Object
-            );
+            )
+            {
+                EmailAddress = firebaseAuth.User.Email,
+                UserPassword = "theday"
+            };
 
-            loginViewModel.EmailAddress = "c@test.com";
-            loginViewModel.UserPassword = "theday";
+            sut.LoginUserCommand.Execute();
 
-            loginViewModel.LoginUserCommand.Execute();
-            _mvxNavigationService.Verify(x => x.Navigate<FriendListViewModel, User>(It.IsAny<User>(), null));
+            _firebaseAuthService.Verify(v => v.LoginUser(It.Is<string>(i => i == sut.EmailAddress), It.Is<string>(i => i == sut.UserPassword)));
+            _userRepository.Verify(v => v.GetUser(It.Is<string>(i => i == firebaseAuth.User.Email)));
+            _mvxNavigationService.Verify(x => x.Navigate<FriendListViewModel, FriendNavUser>(It.Is<FriendNavUser>(i => i == user), null));
         }
     }
 }
