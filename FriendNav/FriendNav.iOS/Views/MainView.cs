caed1a20db3d784f37e9e0f09043bf211024eac1
scch1002 <chrisschwan8009@hotmail.com>
2018-02-25 18:12:07 -0600

    Initial ios view development

---
+++ FriendNav/FriendNav.iOS/Views/MainView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using Foundation;
+using FriendNav.Core.ViewModels;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
+using UIKit;
+
+namespace FriendNav.iOS.Views
+{
+    [MvxFromStoryboard]
+    public partial class MainView : MvxViewController
+    {
+        public MainView(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var set = this.CreateBindingSet<MainView, MainViewModel>();
+            set.Bind(LoginButton)
+                .To(t => t.LoginUserCommand);
+            set.Apply();
+        }
+    }
+}
---
+++ FriendNav/FriendNav.iOS/Views/MainView.designer.cs
@@ -0,0 +1,28 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace FriendNav.iOS.Views
+{
+    [Register ("MainView")]
+    partial class MainView
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LoginButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (LoginButton != null) {
+                LoginButton.Dispose ();
+                LoginButton = null;
+            }
+        }
+    }
+}
