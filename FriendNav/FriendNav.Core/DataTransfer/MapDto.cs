76e1bcb448deb5236d513ef76fdf7d0991716178
Christopher Schwan <chris.schwan@outlook.com>
2018-03-25 10:07:37 -0500

    Update MapDto.cs

--- FriendNav/FriendNav.Core/DataTransfer/MapDto.cs
+++ FriendNav/FriendNav.Core/DataTransfer/MapDto.cs
@@ -7,16 +7,12 @@ namespace FriendNav.Core.DataTransfer
 {
     public class MapDto
     {
-        [JsonProperty("InitiatorLatitude")]
         public string InitiatorLatitude { get; set; }
 
-        [JsonProperty("InitiatorLongitude")]
         public string InitiatorLongitude { get; set; }
 
-        [JsonProperty("ResponderLatitude")]
         public string ResponderLatitude { get; set; }
 
-        [JsonProperty("ResponderLongitude")]
         public string ResponderLongitude { get; set; }
     }
 }
