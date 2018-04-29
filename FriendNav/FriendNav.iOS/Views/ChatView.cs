ad031c60373aedb1cb9b23dfe22769f586c9dafd
pietore <ungericw@tutanota.com>
2018-04-29 14:04:51 -0500

    chat view

---
+++ FriendNav/FriendNav.iOS/Views/ChatView.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Drawing;
+
+using Foundation;
+using MvvmCross.iOS.Views;
+using UIKit;
+
+namespace FriendNav.iOS.Views
+{
+    [MvxFromStoryboard]
+    public partial class ChatView : MvxViewController
+    {
+        public ChatView(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+    }
+}
